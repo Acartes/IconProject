f7a74c982c1df61a95e17ec2fa2a0cde83cb6956
arthur <arthurllorens@hotmail.fr>
2020-11-11 21:46:09 +0100

    Quickfix in nodes

--- Assets/Code/StoryCreator.cs
+++ Assets/Code/StoryCreator.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 
 public class StoryCreator : MonoBehaviour
 {
@@ -13,14 +14,37 @@ public class StoryCreator : MonoBehaviour
     public static StoryCreator Instance;
     public StoryBadge badges;
 
+    public List<ChoiceNode> firstChoices = new List<ChoiceNode>();
+
     void Start()
     {
         Instance = this;
         //DisplayCurrentStory(Color.white);
     }
 
+    bool firstChoice = false;
+    void FirstChoice(Icon choice)
+    {
+        foreach (var item in firstChoices)
+        {
+            if (item.iconRef == choice)
+            {
+                storyNode = item.nextNode;
+                break;
+            }
+        }
+    }
+
+
     public void IconChosen(Icon choice, Color choiceColor)
     {
+        if (!firstChoice)
+        {
+            FirstChoice(choice);
+            firstChoice = true;
+            DisplayCurrentStory(choiceColor);
+            return;
+        }
         foreach (var item in storyNode.choices)
         {
             if (item.iconRef == choice)
@@ -28,7 +52,8 @@ public class StoryCreator : MonoBehaviour
                 storyNode = item.nextNode;
                 DisplayCurrentStory(choiceColor);
                 //DisplayHistory(choiceColor);
-                if(storyNode.choices.Count == 0){
+                if (storyNode.choices.Count == 0)
+                {
                     SendChoice.IconSelectable = false;
                     badges.CheckForBadge(item.nextNode);
                 }
