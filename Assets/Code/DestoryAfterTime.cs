86302d91426c0e15118f2cffae005153d229f678
linusgaertig <linus.gaertig@gmail.com>
2020-11-12 10:42:21 +0100

    Added sounds, export test

---
+++ Assets/Code/DestoryAfterTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryAfterTime : MonoBehaviour
+{
+    public float delay = 1f;
+    void Start()
+    {
+        Destroy(this.gameObject, delay);
+    }
+}
---
+++ Assets/Code/PlaySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public GameObject popSound = default;
+    public float maxPitch = 0.1f;
+
+    public static PlaySound instance = default;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void PlayPopSound()
+    {
+        GameObject audioSource = Instantiate(popSound, this.transform.position, Quaternion.identity);
+        audioSource.transform.SetParent(this.transform);
+        AudioSource curSource = audioSource.GetComponent<AudioSource>();
+        curSource.pitch = 1 + Random.Range(-maxPitch, maxPitch);
+        curSource.Play();
+    }
+}
